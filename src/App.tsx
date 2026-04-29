1eb2a7cf08d242f5ac90da9d56e64bc7cabc64b4
moawedmohamed <moawedmohamed144@gmail.com>
2026-04-29 19:35:04 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<div className="min-h-screen flex flex-col bg-black text-white/95 items-center justify-center text-2xl font-bold text-center">
+			hello react from circleCi 
+		</div>
+	);
+};
+
+export default App;
