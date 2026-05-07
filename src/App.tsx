d785b99bd5a311955adc76391c4299e82058f3eb
moawedmohamed <moawedmohamed144@gmail.com>
2026-05-07 03:02:58 +0300

    update content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
 	return (
 		<div className="min-h-screen flex flex-col bg-black text-white/95 items-center justify-center text-2xl font-bold text-center">
-			hello react from circleCi 
+			hello react from circleCi and new version
 		</div>
 	);
 };
